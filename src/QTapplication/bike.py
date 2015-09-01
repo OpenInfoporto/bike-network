58467b8bac7248e39d4c4f4738bc5b98dd39fd44
Elio Castaldi <elio.castaldi@infoporto.it>
2015-09-01 18:30:36 +0200

    Bigger font for WelcomeCard input

--- src/QTapplication/bike.py
+++ src/QTapplication/bike.py
@@ -48,113 +48,111 @@ class Rest(object):
 
 #pagina web
 class WebTab(QtGui.QWidget):
-  def __init__(self, parent=None):
-    super(WebTab, self).__init__(parent)
-    self.ui = Ui_HttpWidget()
-    self.ui.setupUi(self)
-    
-    # set margins
-    '''
-    l = self.layout()
-    l.setMargin(0)
-    self.ui.horizontalLayout.setMargin(6)
-    '''
-    
-    # set the default page
-    url = 'http://google.it'
-    self.ui.url.setText(url)
-    
-    # load page
-    self.ui.webView.setUrl(QtCore.QUrl(url))
-    
-    # history buttons:
-    self.ui.back.setEnabled(False)
-    self.ui.next.setEnabled(False)
-    
-    #stop button:
-    self.ui.stop.setEnabled(False)
+    def __init__(self, parent=None):
+        super(WebTab, self).__init__(parent)
+        self.ui = Ui_HttpWidget()
+        self.ui.setupUi(self)
+        
+        # set margins
+        '''
+        l = self.layout()
+        l.setMargin(0)
+        self.ui.horizontalLayout.setMargin(6)
+        '''
+        
+        # set the default page
+        url = 'http://google.it'
+        self.ui.url.setText(url)
+        
+        # load page
+        self.ui.webView.setUrl(QtCore.QUrl(url))
+        
+        # history buttons:
+        self.ui.back.setEnabled(False)
+        self.ui.next.setEnabled(False)
+        
+        #stop button:
+        self.ui.stop.setEnabled(False)
 
-    QtCore.QObject.connect(self.ui.back,QtCore.SIGNAL("clicked()"), self.back)
-    QtCore.QObject.connect(self.ui.next,QtCore.SIGNAL("clicked()"), self.next)
-    QtCore.QObject.connect(self.ui.url,QtCore.SIGNAL("returnPressed()"), self.url_changed)
-    QtCore.QObject.connect(self.ui.webView,QtCore.SIGNAL("linkClicked (const QUrl&)"), self.link_clicked)
-    QtCore.QObject.connect(self.ui.webView,QtCore.SIGNAL("urlChanged (const QUrl&)"), self.link_clicked)
-    QtCore.QObject.connect(self.ui.webView,QtCore.SIGNAL("titleChanged (const QString&)"), self.title_changed)
-    QtCore.QObject.connect(self.ui.reload,QtCore.SIGNAL("clicked()"), self.reload_page)
-    QtCore.QObject.connect(self.ui.stop,QtCore.SIGNAL("clicked()"), self.stop_page)
+        QtCore.QObject.connect(self.ui.back,QtCore.SIGNAL("clicked()"), self.back)
+        QtCore.QObject.connect(self.ui.next,QtCore.SIGNAL("clicked()"), self.next)
+        QtCore.QObject.connect(self.ui.url,QtCore.SIGNAL("returnPressed()"), self.url_changed)
+        QtCore.QObject.connect(self.ui.webView,QtCore.SIGNAL("linkClicked (const QUrl&)"), self.link_clicked)
+        QtCore.QObject.connect(self.ui.webView,QtCore.SIGNAL("urlChanged (const QUrl&)"), self.link_clicked)
+        QtCore.QObject.connect(self.ui.webView,QtCore.SIGNAL("titleChanged (const QString&)"), self.title_changed)
+        QtCore.QObject.connect(self.ui.reload,QtCore.SIGNAL("clicked()"), self.reload_page)
+        QtCore.QObject.connect(self.ui.stop,QtCore.SIGNAL("clicked()"), self.stop_page)
 
-    QtCore.QMetaObject.connectSlotsByName(self)
+        QtCore.QMetaObject.connectSlotsByName(self)
   
-  def url_changed(self):
-    #Url have been changed by user
-    page = self.ui.webView.page()
-    history = page.history()
-    if history.canGoBack():
-      self.ui.back.setEnabled(True)
-    else:
-      self.ui.back.setEnabled(False)
-    if history.canGoForward():
-      self.ui.next.setEnabled(True)
-    else:
-      self.ui.next.setEnabled(False)
+    def url_changed(self):
+        #Url have been changed by user
+        page = self.ui.webView.page()
+        history = page.history()
+        if history.canGoBack():
+          self.ui.back.setEnabled(True)
+        else:
+          self.ui.back.setEnabled(False)
+        if history.canGoForward():
+          self.ui.next.setEnabled(True)
+        else:
+          self.ui.next.setEnabled(False)
 
+        url = str(self.ui.url.text())
+        #bug fix: it loads urls with no http
+        if(str(self.ui.url.text())[0:3] != "http"):
+            self.ui.url.setText("http://"+url)
+        
+        url = self.ui.url.text()
+        self.ui.webView.setUrl(QtCore.QUrl(url))
     
-
-    url = str(self.ui.url.text())
-    #bug fix: it loads urls with no http
-    if(str(self.ui.url.text())[0:3] != "http"):
-        self.ui.url.setText("http://"+url)
-    
-    url = self.ui.url.text()
-    self.ui.webView.setUrl(QtCore.QUrl(url))
-    
-  def stop_page(self):
-    #Stop loading the page
-    self.close()
+    def stop_page(self):
+        #Stop loading the page
+        self.close()
   
-  def title_changed(self, title):
-    #Web page title changed - change the tab name
-    self.setWindowTitle(title)
+    def title_changed(self, title):
+        #Web page title changed - change the tab name
+        self.setWindowTitle(title)
   
-  def reload_page(self):
-    #Reload the web page
-    self.ui.webView.setUrl(QtCore.QUrl(self.ui.url.text()))
+    def reload_page(self):
+        #Reload the web page
+        self.ui.webView.setUrl(QtCore.QUrl(self.ui.url.text()))
     
   
-  def link_clicked(self, url):
-    #Update the URL if a link on a web page is clicked
-    page = self.ui.webView.page()
-    history = page.history()
-    if history.canGoBack():
-      self.ui.back.setEnabled(True)
-    else:
-      self.ui.back.setEnabled(False)
-    if history.canGoForward():
-      self.ui.next.setEnabled(True)
-    else:
-      self.ui.next.setEnabled(False)
+    def link_clicked(self, url):
+        #Update the URL if a link on a web page is clicked
+        page = self.ui.webView.page()
+        history = page.history()
+        if history.canGoBack():
+          self.ui.back.setEnabled(True)
+        else:
+          self.ui.back.setEnabled(False)
+        if history.canGoForward():
+          self.ui.next.setEnabled(True)
+        else:
+          self.ui.next.setEnabled(False)
+        
+        self.ui.url.setText(url.toString())
     
-    self.ui.url.setText(url.toString())
-    
-  def back(self):
-    #Back button clicked, go one page back
-    page = self.ui.webView.page()
-    history = page.history()
-    history.back()
-    if history.canGoBack():
-      self.ui.back.setEnabled(True)
-    else:
-      self.ui.back.setEnabled(False)
+    def back(self):
+        #Back button clicked, go one page back
+        page = self.ui.webView.page()
+        history = page.history()
+        history.back()
+        if history.canGoBack():
+          self.ui.back.setEnabled(True)
+        else:
+          self.ui.back.setEnabled(False)
   
-  def next(self):
-    #Next button clicked, go to next page
-    page = self.ui.webView.page()
-    history = page.history()
-    history.forward()
-    if history.canGoForward():
-      self.ui.next.setEnabled(True)
-    else:
-      self.ui.next.setEnabled(False)
+    def next(self):
+        #Next button clicked, go to next page
+        page = self.ui.webView.page()
+        history = page.history()
+        history.forward()
+        if history.canGoForward():
+          self.ui.next.setEnabled(True)
+        else:
+          self.ui.next.setEnabled(False)
       
 #popup
 class MyPopup(QWidget):
@@ -237,8 +235,11 @@ class MainTab(QtGui.QWidget):
 
         #key
         self.welcomeCard = QLineEdit(self)
-        self.welcomeCard.resize(QSize(400,30))
-        self.welcomeCard.move((resolution.width() / 2) - (self.frameSize().width() / 2),(resolution.height() / 0.95) - (self.frameSize().height() / 0.95))
+        self.welcomeCard.resize(QSize(400,60))
+        fontCard = QFont("Sans Serif", 35, QFont.Bold)
+        self.welcomeCard.setFont(fontCard)
+        self.welcomeCard.setAlignment(QtCore.Qt.AlignCenter)
+        self.welcomeCard.move((resolution.width() / 2) - (self.frameSize().width() / 2),(resolution.height() / 0.98) - (self.frameSize().height() / 0.98))
 
         #in button
         self.btn2 = QPushButton( self)
